469b1c2d6a505d86e32f65c14ff880ee744dc99d
urkeev14 <uros.veljkovic1996@gmail.com>
2020-05-02 20:39:39 +0200

    Simple app done

---
+++ CommandDP/src/command/history/CommandHistory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command.history;
+
+import command.Command;
+import java.util.Stack;
+
+/**
+ *
+ * @author urosv
+ */
+public class CommandHistory {
+    
+    private Stack<Command> history = new Stack<>();
+    
+    public void push(Command c){
+        history.push(c);
+    }
+    public Command pop(){
+        return history.pop();
+    }
+    
+    public boolean isEmpty(){
+        return history.isEmpty();
+    }
+}
--- CommandDP/src/forms/EditorGUI.java
+++ CommandDP/src/forms/EditorGUI.java
@@ -5,6 +5,12 @@
  */
 package forms;
 
+import command.Command;
+import command.history.CommandHistory;
+import command.impl.CopyCommand;
+import command.impl.CutCommand;
+import command.impl.PasteCommand;
+
 /**
  *
  * @author urosv
@@ -12,7 +18,8 @@ package forms;
 public class EditorGUI extends javax.swing.JFrame {
 
     public String clipboard;
-    
+    private CommandHistory history = new CommandHistory();
+
     /**
      * Creates new form EditorGUI
      */
@@ -32,28 +39,84 @@ public class EditorGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jbtnCopy1 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea = new javax.swing.JTextArea();
+        jPanel2 = new javax.swing.JPanel();
+        jbtnCopy = new javax.swing.JButton();
+        jbtnPaste = new javax.swing.JButton();
         jbtnUndo = new javax.swing.JButton();
         jbtnCut = new javax.swing.JButton();
-        jbtnCut1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Text editor");
         setResizable(false);
 
-        jbtnCopy1.setText("Ctrl + C");
+        jPanel1.setBackground(new java.awt.Color(241, 242, 242));
+        jPanel1.setAutoscrolls(true);
 
         jTextArea.setColumns(20);
+        jTextArea.setLineWrap(true);
         jTextArea.setRows(5);
         jScrollPane2.setViewportView(jTextArea);
 
+        jbtnCopy.setBackground(new java.awt.Color(76, 155, 247));
+        jbtnCopy.setText("Ctrl + C");
+        jbtnCopy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnCopyActionPerformed(evt);
+            }
+        });
+
+        jbtnPaste.setBackground(new java.awt.Color(255, 220, 91));
+        jbtnPaste.setText("Ctrl + V");
+        jbtnPaste.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnPasteActionPerformed(evt);
+            }
+        });
+
+        jbtnUndo.setBackground(new java.awt.Color(214, 64, 71));
         jbtnUndo.setText("Ctrl + Z");
+        jbtnUndo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnUndoActionPerformed(evt);
+            }
+        });
 
+        jbtnCut.setBackground(new java.awt.Color(76, 155, 247));
         jbtnCut.setText("Ctrl + X");
+        jbtnCut.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnCutActionPerformed(evt);
+            }
+        });
 
-        jbtnCut1.setText("Ctrl + V");
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jbtnCopy)
+                .addGap(18, 18, 18)
+                .addComponent(jbtnCut)
+                .addGap(18, 18, 18)
+                .addComponent(jbtnPaste)
+                .addGap(18, 18, 18)
+                .addComponent(jbtnUndo)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbtnCopy)
+                    .addComponent(jbtnCut)
+                    .addComponent(jbtnPaste)
+                    .addComponent(jbtnUndo))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -63,14 +126,7 @@ public class EditorGUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 495, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jbtnCopy1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jbtnCut)
-                        .addGap(18, 18, 18)
-                        .addComponent(jbtnCut1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jbtnUndo)))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -79,11 +135,7 @@ public class EditorGUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbtnCopy1)
-                    .addComponent(jbtnCut)
-                    .addComponent(jbtnCut1)
-                    .addComponent(jbtnUndo))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -94,12 +146,12 @@ public class EditorGUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(16, 16, 16)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(11, Short.MAX_VALUE))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(13, Short.MAX_VALUE)
+                .addContainerGap(20, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -107,62 +159,49 @@ public class EditorGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EditorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EditorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EditorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EditorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jbtnCopyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnCopyActionPerformed
+        executeCommand(new CopyCommand(this));
+    }//GEN-LAST:event_jbtnCopyActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new EditorGUI().setVisible(true);
-            }
-        });
-    }
-    
-    public String getText(){
-        return this.jTextArea.getText();
-    }
-    
-    public String getSelectedText(){
-        return this.jTextArea.getSelectedText();
-    }
+    private void jbtnCutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnCutActionPerformed
+        executeCommand(new CutCommand(this));
+    }//GEN-LAST:event_jbtnCutActionPerformed
+
+    private void jbtnPasteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnPasteActionPerformed
+        executeCommand(new PasteCommand(this));
+    }//GEN-LAST:event_jbtnPasteActionPerformed
+
+    private void jbtnUndoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnUndoActionPerformed
+        undo();
+    }//GEN-LAST:event_jbtnUndoActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextArea;
-    private javax.swing.JButton jbtnCopy1;
+    private javax.swing.JButton jbtnCopy;
     private javax.swing.JButton jbtnCut;
-    private javax.swing.JButton jbtnCut1;
+    private javax.swing.JButton jbtnPaste;
     private javax.swing.JButton jbtnUndo;
     // End of variables declaration//GEN-END:variables
 
+    public String getText() {
+        return this.jTextArea.getText();
+    }
+
     public void setText(String backup) {
         this.jTextArea.setText(backup);
+    }
+
+    public void appendText(String clipboard) {
+        this.jTextArea.insert(clipboard, this.jTextArea.getCaretPosition());
+    }
+
+    public String getSelectedText() {
+        return this.jTextArea.getSelectedText();
     }
 
     public boolean isClipboardInitialized() {
@@ -171,10 +210,6 @@ public class EditorGUI extends javax.swing.JFrame {
 
     public boolean isClipboardEmpty() {
         return clipboard.isEmpty();
-    }
-
-    public void appendText(String clipboard) {
-        this.jTextArea.insert(clipboard, this.jTextArea.getCaretPosition());
     }
 
     public boolean textNotSelected() {
@@ -188,9 +223,25 @@ public class EditorGUI extends javax.swing.JFrame {
     public int getSelectionStart() {
         return this.jTextArea.getSelectionStart();
     }
-    
-    
+
     public int getSelectionEnd() {
         return this.jTextArea.getSelectionEnd();
+    }
+
+    private void executeCommand(Command command) {
+        if (command.execute()) {
+            history.push(command);
+        }
+    }
+
+    private void undo() {
+        if (history.isEmpty()) {
+            return;
+        }
+
+        Command command = history.pop();
+        if (command != null) {
+            command.undo();
+        }
     }
 }
